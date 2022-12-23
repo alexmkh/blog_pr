e4fbb10796f6d57d9008f37cc598f1aec6b580d2
Alexander Kharshakh <alexmkh33@gmail.com>
2022-12-23 16:33:58 +0200

    JS OK

--- posts/models.py
+++ posts/models.py
@@ -4,6 +4,7 @@ from tinymce import models as tinymce_models
 class Post(models.Model):
     title = models.TextField(max_length=100)
     text = tinymce_models.HTMLField()
+    img = models.ImageField(blank=True, null=True)
     
     def __str__(self):
         return self.title
